72c7d47f17e9f362d795259e7733521ea4e490fe
Fields Marshall <fields.marshall@gmail.com>
2025-04-07 13:39:56 -0400

    config mediastore

--- tina/config.js
+++ tina/config.js
@@ -593,25 +593,28 @@ const schema = defineSchema({
   ],
 });
 
-const { LimitedSizeMediaStore } = require("./media/CustomMediaStore");
-
-const config = defineConfig({
+export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
   branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH ||
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
-    process.env.HEAD,
+    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
+    process.env.HEAD, // Netlify branch env
   token: process.env.TINA_TOKEN,
   media: {
-    loadCustomStore: async (args) => {
-      const originalStore = await args.originalMediaStore.load();
-      const cms = args.cms; // <-- get TinaCMS instance
-      return new LimitedSizeMediaStore(originalStore, cms);
+    // If you wanted cloudinary do this
+    // loadCustomStore: async () => {
+    //   const pack = await import("next-tinacms-cloudinary");
+    //   return pack.TinaCloudCloudinaryMediaStore;
+    // },
+    // this is the config for the tina cloud media store
+    tina: {
+      publicFolder: "public",
+      mediaRoot: "uploads",
     },
   },
   build: {
-    publicFolder: "public",
-    outputFolder: "admin",
+    publicFolder: "public", // The public asset folder for your framework
+    outputFolder: "admin", // within the public folder
   },
   schema,
 });
